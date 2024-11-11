a270540ce7d3d4635f3142bb0a16d8f01f387fef
Rail Neira <r.neira03@ufromail.cl>
2024-11-10 23:14:17 -0300

    Agrega graficos de datos de cada measure

--- src/views/components/JobRunTimeline.vue
+++ src/views/components/JobRunTimeline.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="container my-3">
-        <div v-for="(measure, index) in measures" :key="index" :class="'row pa-5 ' + (index % 2 ? 'even-row' : 'odd-row')">
+        <div v-for="(measure, index) in measures" :key="index"
+            :class="'row pa-5 ' + (index % 2 ? 'even-row' : 'odd-row')">
             <div class="col-auto d-flex align-items-start mt-10">
                 <h5>{{ index }}</h5>
             </div>
@@ -24,7 +25,7 @@
     </div>
 
     <!-- Modal para crear o editar job -->
-    
+
     <div class="modal fade" id="measureModal" tabindex="-1" aria-labelledby="measureModal" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -35,22 +36,29 @@
                 <div class="modal-body">
                     <div class="container">
                         <div class="row">
-                            <div class="col">
-                                <p>Duration: {{ selectedMeasure.duration }} seconds</p>
-                                <p>Vectors: {{ selectedMeasure.vectors }}</p>
+                            <div class="col-9">
+                                <div class="row pa-5">
+                                    <div class="col">
+                                        <real-time-chart :data="selectedMeasure.accelData"
+                                            height="330"></real-time-chart>
+                                    </div>
+                                </div>
+                                <div class="row pa-5">
+                                    <div class="col">
+                                        <real-time-chart :data="selectedMeasure.gyroData"
+                                            height="330"></real-time-chart>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col">
-                                <p>Duration: {{ selectedMeasure.duration }} seconds</p>
-                                <p>Vectors: {{ selectedMeasure.vectors }}</p>
-                            </div>
-
-                            <div class="col">
-                                <p>Duration: {{ selectedMeasure.duration }} seconds</p>
-                                <p>Vectors: {{ selectedMeasure.vectors }}</p>
-                            </div>
-                            <div class="col">
-                                <p>Duration: {{ selectedMeasure.duration }} seconds</p>
-                                <p>Vectors: {{ selectedMeasure.vectors }}</p>
+                            <div class="col-3">
+                                <div class="row">
+                                    <h5>Información sobre la measure</h5>
+                                    <div class="d-flex flex-column align-items-center pt-15 text-muted">
+                                        <i class="information-off-outline" style="font-size: 2rem;"></i>
+                                        <svg-icon type="mdi" :path="path"></svg-icon>
+                                        <p class="text-center mt-2">No hay análisis disponible para estos datos.</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -63,18 +71,76 @@
 <script>
 import { useNodeStore } from "@/stores/nodeStore.js";
 import measuresService from "@/services/measures.service.js";
+import RealTimeChart from '@/views/RealTimeChart.vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiInformationOffOutline } from '@mdi/js';
 export default {
     props: ["jobrunid"],
+    components: {
+        RealTimeChart,
+        SvgIcon,
+    },
     data() {
         return {
+            path: mdiInformationOffOutline,
             measures: [],
             nodeStore: useNodeStore(),
-            selectedMeasure: {}
+            selectedMeasure: {
+                _id: "",
+                accelData: {
+                    t: [],
+                    x: [],
+                    y: [],
+                    z: [],
+                },
+                gyroData: {
+                    t: [],
+                    x: [],
+                    y: [],
+                    z: [],
+                },
+            },
+
         };
     },
     methods: {
         viewMeasure(index) {
-            this.selectedMeasure = this.measures[index];
+            // Se debe cambiar la data para los gráficos sin que se pierda la referencia, 
+            // por eso solo se pueden usar métodos de mutación de los arrays como push, pop, shift, etc.
+
+            // clear previous data, method by https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript
+            this.selectedMeasure.accelData.t.length = 0;
+            this.selectedMeasure.accelData.x.length = 0;
+            this.selectedMeasure.accelData.y.length = 0;
+            this.selectedMeasure.accelData.z.length = 0;
+
+            this.selectedMeasure.gyroData.t.length = 0;
+            this.selectedMeasure.gyroData.x.length = 0;
+            this.selectedMeasure.gyroData.y.length = 0;
+            this.selectedMeasure.gyroData.z.length = 0;
+
+            // set the new data
+            this.selectedMeasure._id = this.measures[index]._id;
+
+            let t = Array.from({ length: this.measures[index].vectors }, (_, i) => i + 1);
+            let x = this.measures[index].data.map((v) => v.ax);
+            let y = this.measures[index].data.map((v) => v.ay);
+            let z = this.measures[index].data.map((v) => v.az);
+
+            this.selectedMeasure.accelData.t.push(...t);
+            this.selectedMeasure.accelData.x.push(...x);
+            this.selectedMeasure.accelData.y.push(...y);
+            this.selectedMeasure.accelData.z.push(...z);
+
+            let gx = this.measures[index].data.map((v) => v.gx);
+            let gy = this.measures[index].data.map((v) => v.gy);
+            let gz = this.measures[index].data.map((v) => v.gz);
+
+            this.selectedMeasure.gyroData.t.push(...t);
+            this.selectedMeasure.gyroData.x.push(...gx);
+            this.selectedMeasure.gyroData.y.push(...gy);
+            this.selectedMeasure.gyroData.z.push(...gz);
+
             new bootstrap.Modal(document.getElementById('measureModal')).show();
         },
     },
@@ -98,6 +164,7 @@ export default {
                     measures[i].vectors = 0;
                     measures[i].duration = 0;
                 }
+                measures[i].data = data.slice(1, data.length - 1);
             }
         }
         this.measures = measures;
